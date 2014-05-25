6f43c4552d494dae2c896d88644648361d367045
Lucjan Suski <lucjansuski@gmail.com>
2014-05-25 11:50:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindMe::Application.config.secret_key_base = '58a3f617c2b88a006f64836801eb06107a30ffb10312d62cbccd952939a162bf5992f2628f8f7c0f798dff81b97fa24b33d92bbf03761e7997e7a646cbf28349'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindMe::Application.config.session_store :cookie_store, key: '_find_me_session'
